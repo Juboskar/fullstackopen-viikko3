dd855a0f4d1023bfb5ebde131125afdf1f48f611
Juboskar <oskari.juvakka@gmail.com>
2022-05-10 18:26:34 +0300

    2.22

--- index.js
+++ index.js
@@ -12,101 +12,89 @@ app.use(morgan(':method :url :status :res[content-length] - :response-time ms :d
 app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('<h1>Hello World!</h1>')
+  res.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/persons', (req, res) => {
-    Person.find({}).then(result => {
-        res.json(result)
-    })
+  Person.find({}).then(result => {
+    res.json(result)
+  })
 })
 
 app.get('/api/persons/:id', (req, res, next) => {
-    Person.find({ id: req.params.id }).then(result => {
-        if (result.length > 0) {
-            res.json(result)
-        } else {
-            throw new Error('not found')
-        }
-    }).catch(error => next(error))
+  Person.find({ id: req.params.id }).then(result => {
+    if (result.length > 0) {
+      res.json(result)
+    } else {
+      throw new Error('not found')
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id', (req, res, next) => {
-    Person.findOneAndRemove({ id: req.params.id })
-        .then(result => {
-            if (result) {
-                res.status(204).end()
-            } else {
-                throw new Error('id not found')
-            }
-        })
-        .catch(error => next(error))
+  Person.findOneAndRemove({ id: req.params.id })
+    .then(result => {
+      if (result) {
+        res.status(204).end()
+      } else {
+        throw new Error('id not found')
+      }
+    })
+    .catch(error => next(error))
 })
 
 
 app.get('/info', (req, res) => {
-    Person.find({}).then(result => {
-        res.send(`<div>Phonebook has info for ${result.length} people</div>
+  Person.find({}).then(result => {
+    res.send(`<div>Phonebook has info for ${result.length} people</div>
     <div>${new Date()}</div>`)
-    })
+  })
 })
 
 app.post('/api/persons', (req, res, next) => {
-    Person.find({}).then(result => {
-        const persons = result
-        if (!req.body.name) {
-            throw new Error('name missing')
-        }
+  Person.find({}).then(result => {
+    const persons = result
 
-        if (!req.body.number) {
-            throw new Error('number missing')
-        }
-
-        if (persons.map(p => p.name).includes(req.body.name)) {
-            throw new Error('name must be unique')
-        }
-
-        let newId = null
-        while (persons.map(p => p.id).includes(newId) || newId === null) {
-            newId = Math.floor(Math.random() * 1000000)
-        }
-        const person = new Person({
-            name: req.body.name,
-            number: req.body.number,
-            id: newId
-        })
-        person.save().then(p => {
-            res.json(p)
-        }).catch(error => next(error))
+    let newId = null
+    while (persons.map(p => p.id).includes(newId) || newId === null) {
+      newId = Math.floor(Math.random() * 1000000)
+    }
+    const person = new Person({
+      name: req.body.name,
+      number: req.body.number,
+      id: newId
+    })
+    person.save().then(p => {
+      res.json(p)
     }).catch(error => next(error))
+  }).catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (req, res, next) => {
-    const person = {
-        name: req.body.name,
-        number: req.body.number
-    }
+  const person = {
+    name: req.body.name,
+    number: req.body.number
+  }
 
-    Person.findOneAndUpdate({ id: req.params.id }, person,
-        { new: true, runValidators: true, context: 'query' })
-        .then(updated => {
-            res.json(updated)
-        }).catch(error => next(error))
+  Person.findOneAndUpdate({ id: req.params.id }, person,
+    { new: true, runValidators: true, context: 'query' })
+    .then(updated => {
+      res.json(updated)
+    }).catch(error => next(error))
 })
 
 const port = process.env.PORT || 3001
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`)
+  console.log(`Server running on port ${port}`)
 })
 
 const errorHandler = (error, req, res, next) => {
-    console.log("jou")
-    console.log(error.name)
 
+  if (error.name === 'ValidationError') {
     return res.status(400).json({ error: error.message })
+  }
 
-
-    next(error)
+  next(error)
 }
 
 app.use(errorHandler)
--- mongo.js
+++ mongo.js
@@ -2,8 +2,8 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 
@@ -13,34 +13,34 @@ const url = process.env.MONGODB_URI
 mongoose.connect(url)
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
 const Person = mongoose.model('Person', personSchema)
 
 if (process.argv.length === 3) {
-    console.log('phonebook: ')
-    Person.find({}).then(result => {
-        result.forEach(p => {
-            console.log(p.name + ' ' + p.number)
-        })
-        mongoose.connection.close()
+  console.log('phonebook: ')
+  Person.find({}).then(result => {
+    result.forEach(p => {
+      console.log(p.name + ' ' + p.number)
     })
+    mongoose.connection.close()
+  })
 }
 
 else {
 
-    const name = process.argv[3]
-    const number = process.argv[4]
+  const name = process.argv[3]
+  const number = process.argv[4]
 
-    const note = new Person({
-        name: name,
-        number: number,
-    })
+  const note = new Person({
+    name: name,
+    number: number,
+  })
 
-    note.save().then(result => {
-        console.log(`added ${name} number ${number}`)
-        mongoose.connection.close()
-    })
+  note.save().then(result => {
+    console.log(`added ${name} number ${number}`)
+    mongoose.connection.close()
+  })
 }
